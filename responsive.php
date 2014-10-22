9bd16dc46461c94281349063372600211a410ddf
Ben Buie <mail@benbuie.com>
2014-10-22 15:02:47 -0600

    added most functionality to one function, changed name to breakpoints

--- responsive.php
+++ responsive.php
@@ -2,138 +2,160 @@
 	
 // remap jQuery to $
 (function($){
+	
+	customApp.breakpointsInit = function()
+		{
+			//object for organizing code
+			var breakpointsInit = new Object();
 
-	//trigger on resize
-	$(window).resize(function() {
-		customApp.viewportWidth = $(window).width();
-		customApp.viewportHeight = $(window).height();
-		customApp.checkBreakpointLoaded();
-		var viewChangeTimeout;
-		if(customApp.currentView != customApp.loadedView) {
-			customApp.loadedView = customApp.currentView;
-			customApp.changeViewLoaded();			
+			breakpointsInit.setup = function()
+				{
+					customApp.viewportWidth = $(window).width();
+					customApp.viewportHeight = $(window).height();
+					customApp.breakPoints = {
+						desktopWide: {
+							maxWidth: 1000000,
+							next: false
+						},
+						desktop: {
+							maxWidth: 1200,
+							next: 'desktopWide'
+						},
+						tabletLandscape: {
+							maxWidth: 1024,
+							next: 'desktop'
+						},
+						tablet: {
+							maxWidth: 768,
+							next: 'tabletLandscape'
+						},
+						phoneLandscape: {
+							maxWidth: 480,
+							next: 'tablet'
+						},
+						phone: {
+							maxWidth: 320,
+							next: 'phoneLandscape'
+						}			
+					}
+					for (var breakpoint in customApp.breakPoints) {
+						if(customApp.viewportWidth <= customApp.breakPoints[breakpoint].maxWidth){
+							customApp.currentView = breakpoint;
+							customApp.loadedView = breakpoint;
+						}
+					}
+					breakpointsInit.resize()
+				}
+			breakpointsInit.checkBreakpointLoaded = function()
+				{
+					for (var breakpoint in customApp.breakPoints) {
+						if(customApp.viewportWidth <= customApp.breakPoints[breakpoint].maxWidth){
+							customApp.currentView = breakpoint;				
+						}
+					}
+					if($('.ie8').length){
+						customApp.currentView = 'desktop';
+					}
+				}
+			breakpointsInit.resize = function()
+				{
+					$(window).resize(function() {		
+						customApp.viewportWidth = $(window).width();
+						customApp.viewportHeight = $(window).height();
+						customApp.breakpointsInitObj.checkBreakpointLoaded();
+						if(customApp.currentView != customApp.loadedView) {
+							customApp.loadedView = customApp.currentView;
+							customApp.breakpointsViewChange();			
+						}
+					});
+				}
+			breakpointsInit.setup();
+			customApp.breakpointsInitObj = breakpointsInit;
 		}
-	});
-
-	customApp.changeViewLoaded = function()
+	customApp.breakpointsInit();
+	customApp.breakpointsViewChange = function()
 		{
 			//use this function to perform any changes to the view based on breakpoint changes
-		}
-	customApp.checkBreakpointLoaded = function()
+		}	
+	customApp.breakpointImages = function(callback)
 		{
-			for (var breakpoint in customApp.breakPoints) {
-				if(customApp.viewportWidth < customApp.breakPoints[breakpoint].maxWidth){
-					customApp.currentView = breakpoint;				
-				}
-			}
-			if($('.ie8').length){
-				customApp.currentView = 'desktop';
-			}
-		}		
-	customApp.setViewportVariables = function()
-		{
-			customApp.viewportWidth = $(window).width();
-			customApp.viewportHeight = $(window).height();
-			customApp.breakPoints = {
-				desktopWide: {
-					maxWidth: 1000000,
-					next: false
-				},
-				desktop: {
-					maxWidth: 1200,
-					next: 'desktopWide'
-				},
-				tabletLandscape: {
-					maxWidth: 1024,
-					next: 'desktop'
-				},
-				tablet: {
-					maxWidth: 768,
-					next: 'tabletLandscape'
-				},
-				phoneLandscape: {
-					maxWidth: 480,
-					next: 'tablet'
-				},
-				phone: {
-					maxWidth: 320,
-					next: 'phoneLandscape'
-				}			
-			}
-			for (var breakpoint in customApp.breakPoints) {
-				if(customApp.viewportWidth <= customApp.breakPoints[breakpoint].maxWidth){
-					customApp.currentView = breakpoint;
-					customApp.loadedView = breakpoint;
-				}
-			}
-		}
-	customApp.setViewportVariables();
-	
-	//need to add function for resizing
-	
-	customApp.loadResponsiveImages = function(callback)
-		{
-			$("img[responsiveImg='1']").each(function(){
-				var me = $(this);
-				var srcAttr = 'src-'+customApp.currentView;
+			//object for organizing code
+			var breakpointImages = new Object();
 
-				var src = me.attr(srcAttr);
-				var currentSrc = me.attr('src');
-
-				//loop through all breakpoints to show the smallest image that is bigger than the breakpoint
-				var loopCount = 0;
-				var nextView = customApp.breakPoints[customApp.currentView].next;		
-				while(!src && nextView != false){	
-					//prevent an infinite loop
-					if(loopCount > 10){
-						break;
+			breakpointImages.setup = function()
+				{
+					//load first priority images
+					$("img[responsiveImg='1']").each(function(){
+						var me = $(this);
+						breakpointImages.loadImages(me)
+					});
+					//run callback after first priority images are loaded
+					if(callback){
+						callback();
 					}
-					srcAttr = 'src-'+nextView;
-					src = me.attr(srcAttr);
-					nextView = customApp.breakPoints[nextView].next;
-					loopCount++;
+					//load second priority images
+					$("img[responsiveImg='2']").each(function(){
+						var me = $(this);
+						breakpointImages.loadImages(me)
+					});
 				}
-				if(src && src != currentSrc){
-					me.attr('src',src);
+			breakpointImages.loadImages = function($image)
+				{
+					var srcAttr = 'src-'+customApp.currentView;
+					var src = $image.attr(srcAttr);
+					var currentSrc = $image.attr('src');
+
+					//loop through all breakpoints to show the smallest image that is bigger than the breakpoint
+					var loopCount = 0;
+					var nextView = customApp.breakPoints[customApp.currentView].next;		
+					while(!src && nextView != false){	
+						//prevent an infinite loop
+						if(loopCount > 10){
+							break;
+						}
+						srcAttr = 'src-'+nextView;
+						src = $image.attr(srcAttr);
+						nextView = customApp.breakPoints[nextView].next;
+						loopCount++;
+					}
+					//set the source if it is different from current and if one was found
+					if(src && src != currentSrc){
+						$image.attr('src',src);
+					}
 				}
-			});
-			if(callback){
-				callback();
-			}
+			breakpointImages.emptySrc = function()
+				{					
+					$("img[responsiveImg='1'], img[responsiveImg='2']").each(function(){
+						var me = $(this);
+						me.attr('src', '');
+					});
+				}
+			breakpointImages.setup();
+			customApp.breakpointImagesObj = breakpointImages;		
 		}
 	
 })(window.jQuery);
 </script>
 
-<style>
-/*
- * TABLE OF CONTENTS
- * 
- * tabletPortraitStyles
- * mobileStyles
- */
-
-
-/*
- * tabletPortraitStyles *****************************************
- */
-@media only screen and (max-width: 1023px) {
-}
-
-/*
- * mobileStyles
- */
-@media only screen and (max-width: 767px) {
-}
-
-</style>
-
 
 <!-- start html for responsive img, IMPORTANT! img-desktopWide is the fallback -->
+<!-- responsiveImg = 1 for high priority image -->
 <img 
+	src=''
 	alt='' 
-	responsiveImg='1'
-	src=''							
+	responsiveImg='1'								
+	src-phone=''
+	src-phoneLandscape=''
+	src-tablet=''
+	src-tabletLandscape=''
+	src-desktop=''
+	src-desktopWide=''	
+/>
+<!-- responsiveImg = 2 for low priority image -->
+<img 
+	src=''
+	alt='' 
+	responsiveImg='2'								
 	src-phone=''
 	src-phoneLandscape=''
 	src-tablet=''

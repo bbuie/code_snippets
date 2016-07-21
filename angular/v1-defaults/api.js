5d927cfc481db6c4280bb5a85a4eb58f904da25e
Ben Buie <mail@benbuie.com>
2016-07-21 11:07:10 -0600

    updated api layout

--- angular/v1-defaults/api.js
+++ angular/v1-defaults/api.js
@@ -1,48 +1,55 @@
-/**
- * @ngdoc service
- * @name someApp.someAppApi
- * @description
- * # someAppApi
- * Factory in someApp.
- */
-angular.module('someApp')
-.factory('someAppApi', function ($http) {
-  'use strict';
-  
-  var someAppApi = {};//object returned from factory
-  var apiCache = {};
+(function ($, angular) {
+  /**
+   * @ngdoc service
+   * @name someApp.someAppApi
+   * @description
+   * # someAppApi
+   * Factory in someApp.
+   */
+  angular.module('someApp').factory('someAppApi', someAppApi);
 
-  someAppApi.setup = function(){
-    //setup api cache
-    apiCache.someCache = null;
-  };
-
-  someAppApi.someData = function(someDataId){
-
-    var someData = {};
+  function someAppApi() {
+    'use strict';
     
-    someData.type = function(type){
-      var url = '/api/someData/type/' + type +'/';
-      var type = $http.get(url);
-      return type;
-    };
-    
-    someData.types = function(){
+    var api = {};//object returned from factory
+    var apiCache = {};
 
-      //return cache if exists
-      if(apiCache.someDataTypes){
-        return apiCache.someDataTypes; 
-      }
-
-      var url = '/api/someData/types/';
-      apiCache.someDataTypes = $http.get(url);
-      return apiCache.someDataTypes;
+    api.setup = function(){
+      //setup api cache
+      apiCache.someCache = null;
     };
 
-    return someData;
-  };
+    api.someData = function(someDataId){
 
-  someAppApi.setup();
+      var someData = {};
+      
+      someData.type = function(type){
+        var url = '/api/someData/type/' + type +'/';
+        var type = $http.get(url);
+        return type;
+      };
+      
+      someData.types = function(){
 
-  return someAppApi;
-});
+        //return cache if exists
+        if(apiCache.someDataTypes){
+          return apiCache.someDataTypes; 
+        }
+
+        var url = '/api/someData/types/';
+        apiCache.someDataTypes = $http.get(url);
+        return apiCache.someDataTypes;
+      };
+
+      return someData;
+    };
+
+    api.setup();
+
+    return api;
+  }
+
+  someAppApi.$inject = [
+    '$http',
+  ];
+}(window.jQuery || window.$, window.angular));

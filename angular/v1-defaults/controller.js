b5bbf248476fad3a63449d0604361e0be92f13c4
Ben Buie <mail@benbuie.com>
2017-01-06 16:42:35 -0700

    updated layout of controller

--- angular/v1-defaults/controller.js
+++ angular/v1-defaults/controller.js
@@ -1,55 +1,55 @@
 (function ($, angular) {'use strict';
-/**
- * @ngdoc function
- * @name someApp.controller:DefaultController
- * @description
- */
-angular.module('someApp').controller('DefaultController', DefaultController);
+	/**
+	 * @ngdoc function
+	 * @name someApp.controller:DefaultController
+	 * @description
+	 */
+	angular.module('someApp').controller('DefaultController', DefaultController);
 
-function DefaultController(
-	$scope,
-	$q
-){
-	var vm = this;
+	function DefaultController(
+		$scope,
+		$q
+	){
+		var vm = this;
 
-	function setup(){
+		function setup(){
 
-		//set scope variables here
+			//set scope variables here
 
-		viewApi();
-		onEvents();
-	}
-	function viewApi(){
-		//functions to be used in view can be added to $scope here
-	}
-	function onEvents(){
+			viewApi();
+			onEvents();
+		}
+		function viewApi(){
+			//functions to be used in view can be added to $scope here
+		}
+		function onEvents(){
 
-		var onSomething = $scope.$on('something', function(e, data){
-			//use this to catch broadcasts
-		});
+			var onSomething = $scope.$on('something', function(e, data){
+				//use this to catch broadcasts
+			});
 
-		$scope.$on('$destroy', onSomething);
-	}
-	function getSomething(somethingId){
+			$scope.$on('$destroy', onSomething);
+		}
+		function getSomething(somethingId){
 
-		return $q(getSomethingPromise);
+			return $q(getSomethingPromise);
 
-		function getSomethingPromise(getSomethingResolve, getSomethingReject){
-			api.then(getSomethingSuccess, getSomethingError);
+			function getSomethingPromise(getSomethingResolve, getSomethingReject){
+				api.then(getSomethingSuccess, getSomethingError);
 
-			function getSomethingSuccess(response){
-				getSomethingResolve(response);
-			}
-			function getSomethingError(response){
-				getSomethingReject(response);
+				function getSomethingSuccess(response){
+					getSomethingResolve(response);
+				}
+				function getSomethingError(response){
+					getSomethingReject(response);
+				}
 			}
 		}
+		setup();
 	}
-	setup();
-}
-DefaultController.$inject = [
-  '$scope',
-  '$q'
-];
+	DefaultController.$inject = [
+	  '$scope',
+	  '$q'
+	];
 
 }(window.jQuery || window.$, window.angular));

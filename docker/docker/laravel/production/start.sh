4f97e1a6d3936caefd19b9cd6e87958ed0682f3b
Ben Buie <mail@benbuie.com>
2018-08-04 10:04:54 -0600

    updated settings for pushing a site to production

--- docker/docker/laravel/production/start.sh
+++ docker/docker/laravel/production/start.sh
@@ -7,12 +7,17 @@ if [ -f "$APACHE_PID_FILE" ]; then
     rm "$APACHE_PID_FILE"
 fi
 
-echo "Adding Oauth Keys For Laravel..."
+echo "Adding Oauth keys for Laravel..."
 echo $OAUTH_PUBLIC_KEY > /var/www/html/storage/oauth-public.key
 sed -i 's/\r /\n/g' /var/www/html/storage/oauth-public.key
 echo $OAUTH_PRIVATE_KEY > /var/www/html/storage/oauth-private.key
 sed -i 's/\r /\n/g' /var/www/html/storage/oauth-private.key
-echo "Keys added."
+echo "Oauth keys added."
+
+echo "Adding database key..."
+echo $COMPOSE_PRIVATE_KEY > /etc/ssl/private/compose.key
+sed -i 's/\r /\n/g' /etc/ssl/private/compose.key
+echo "Database key added."
 
 echo "Build the autoload file..."
 composer dump-autoload
@@ -20,7 +25,17 @@ echo "autoload dumped."
 
 echo "Running db migrations..."
 php artisan --verbose migrate --force
-echo "Migrations done."
+if [ $? = 1 ]; then # if migrations failed
+    echo "Migrations failed, alerting webmaster..."
+    curl -s --user "api:${MAILGUN_SECRET}" https://api.mailgun.net/v3/$MAILGUN_DOMAIN/messages \
+        -F from=$WEBMASTER_EMAIL \
+        -F to=$WEBMASTER_EMAIL \
+        -F subject="URGENT: ${APP_NAME} Failed To Migrate" \
+        -F text='Your database may have been partially migrated but your container should not have been replaced.'
+    echo "Migrations failed, exiting..."
+    exit 1
+fi
+echo "Migrations succeeded"
 
 echo "Starting queue listener..."
 php artisan queue:work --tries=3 &
